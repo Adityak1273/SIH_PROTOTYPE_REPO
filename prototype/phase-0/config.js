960a3b98d227af130016a8069b75ec13415a88c9
Aditya Kumar <adityak1273@gmail.com>
2026-09-04 04:17:18 +0530

    feat(phase4): wire production layer into app shell

--- prototype/phase-0/config.js
+++ prototype/phase-0/config.js
@@ -6,9 +6,9 @@ window.CCNER_CONFIG = {
 };
 window.COGNITIVE_AI_ENDPOINT = window.CCNER_CONFIG.AI_ENDPOINT;
 (function(){
-  const v='0.7.0';
-  const css=document.createElement('link'); css.rel='stylesheet'; css.href='./phase2.css?v='+v; document.head.appendChild(css);
-  const css3=document.createElement('link'); css3.rel='stylesheet'; css3.href='./phase3.css?v='+v; document.head.appendChild(css3);
-  const script=document.createElement('script'); script.src='./phase2.js?v='+v; script.defer=true; document.head.appendChild(script);
-  const script3=document.createElement('script'); script3.src='./phase3.js?v='+v; script3.defer=true; document.head.appendChild(script3);
+  const v='0.7.1';
+  const addCss=(name)=>{const el=document.createElement('link');el.rel='stylesheet';el.href='./'+name+'?v='+v;document.head.appendChild(el)};
+  const addScript=(name)=>{const el=document.createElement('script');el.src='./'+name+'?v='+v;el.defer=true;document.head.appendChild(el)};
+  addCss('phase2.css'); addCss('phase3.css'); addCss('phase4.css');
+  addScript('phase2.js'); addScript('phase3.js'); addScript('phase4.js');
 })();
