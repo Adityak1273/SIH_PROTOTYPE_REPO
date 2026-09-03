b2e0431e4686a32641198be8d86c6dc9a0213e20
Aditya Kumar <adityak1273@gmail.com>
2026-09-04 05:00:37 +0530

    Wire Game 3 category matching into app

--- prototype/phase-0/config.js
+++ prototype/phase-0/config.js
@@ -6,9 +6,9 @@ window.CCNER_CONFIG = {
 };
 window.COGNITIVE_AI_ENDPOINT = window.CCNER_CONFIG.AI_ENDPOINT;
 (function(){
-  const v='0.9.2';
+  const v='0.9.3';
   const addCss=(name)=>{const el=document.createElement('link');el.rel='stylesheet';el.href='./'+name+'?v='+v;document.head.appendChild(el)};
   const addScript=(name)=>{const el=document.createElement('script');el.src='./'+name+'?v='+v;document.head.appendChild(el)};
-  addCss('phase2.css'); addCss('phase3.css'); addCss('phase4.css'); addCss('multilingual.css'); addCss('sequence-game.css'); addCss('sorting-game.css');
-  addScript('phase2.js'); addScript('phase3.js'); addScript('phase4.js'); addScript('sequence-game.js'); addScript('sorting-game.js');
+  addCss('phase2.css'); addCss('phase3.css'); addCss('phase4.css'); addCss('multilingual.css'); addCss('sequence-game.css'); addCss('sorting-game.css'); addCss('category-game.css');
+  addScript('phase2.js'); addScript('phase3.js'); addScript('phase4.js'); addScript('sequence-game.js'); addScript('sorting-game.js'); addScript('category-game.js');
 })();
