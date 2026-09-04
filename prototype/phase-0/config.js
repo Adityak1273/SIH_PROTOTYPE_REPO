c97fa146d0ce8f4d84694e86933a5136e28c00d7
Aditya Kumar <adityak1273@gmail.com>
2026-09-05 02:04:40 +0530

    chore: align auth build version

--- prototype/phase-0/config.js
+++ prototype/phase-0/config.js
@@ -3,6 +3,6 @@ window.CCNER_CONFIG = {
   AI_ENDPOINT: 'https://sih-prototype-repo.zopcloud.zop.dev/api/chat',
   SUPABASE_URL: 'https://mmgvgqtjlcrrfkehlqqe.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_cchVUBU7WOnxYH_sI8A1QA_05f1Sz4v',
-  APP_VERSION: '0.18.4'
+  APP_VERSION: '0.18.5'
 };
 window.COGNITIVE_AI_ENDPOINT = window.CCNER_CONFIG.AI_ENDPOINT;
